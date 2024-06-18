c8d67c6520624970c009e9fc3416929e72141582
seakee <seakee23@gmail.com>
2024-06-18 10:06:55 +0800

    Update the unit tests after supporting TraceID.

--- go.mod
+++ go.mod
@@ -4,13 +4,17 @@ go 1.19
 
 require (
 	github.com/lestrrat-go/file-rotatelogs v2.4.0+incompatible
+	github.com/stretchr/testify v1.8.0
 	go.uber.org/zap v1.24.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/jonboulle/clockwork v0.4.0 // indirect
 	github.com/lestrrat-go/strftime v1.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- logger_test.go
+++ logger_test.go
@@ -1,25 +1,118 @@
-package logger
+package logger_test
 
 import (
-	"errors"
-	"go.uber.org/zap"
+	"bytes"
+	"context"
+	"encoding/json"
+	"github.com/sk-pkg/logger"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
-func TestLogger(t *testing.T) {
-	logger, err := New()
+func TestNew(t *testing.T) {
+	// Test creating a new logger manager with default options
+	mgr, err := logger.New()
+	assert.NoError(t, err)
+	assert.NotNil(t, mgr)
 
-	if err != nil {
-		t.Fatal(err)
+	// Test creating a new logger manager with custom options
+	mgr, err = logger.New(
+		logger.WithDriver("file"),
+		logger.WithLogPath("/tmp/test-log-"),
+		logger.WithLevel("debug"),
+	)
+	assert.NoError(t, err)
+	assert.NotNil(t, mgr)
+}
+
+func TestLoggingWithTraceID(t *testing.T) {
+	var buf bytes.Buffer
+	writer := zapcore.AddSync(&buf)
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "T",
+		LevelKey:       "L",
+		NameKey:        "N",
+		CallerKey:      "C",
+		MessageKey:     "M",
+		StacktraceKey:  "S",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
 	}
 
-	logger.Sync()
+	core := zapcore.NewCore(
+		zapcore.NewJSONEncoder(encoderConfig),
+		writer,
+		zapcore.DebugLevel,
+	)
 
-	err = errors.New("This is an error testing logger ")
+	zapLogger := zap.New(core)
+	mgr := &logger.Manager{Zap: zapLogger}
 
-	logger = logger.Named("test")
+	ctx := context.WithValue(context.Background(), logger.TraceIDKey, "test-trace-id")
 
-	logger.Info("Info")
-	logger.Debug("Debug")
-	logger.Error("Error occurs", zap.Error(err))
+	// Test Info level logging
+	mgr.Info(ctx, "This is an info message")
+
+	// Flush and sync the buffer
+	mgr.Sync()
+
+	var loggedMessage map[string]interface{}
+	err := json.Unmarshal(buf.Bytes(), &loggedMessage)
+	assert.NoError(t, err)
+
+	assert.Equal(t, "INFO", loggedMessage["L"])
+	assert.Equal(t, "This is an info message", loggedMessage["M"])
+	assert.Equal(t, "test-trace-id", loggedMessage["TraceID"])
+}
+
+func TestLoggingLevels(t *testing.T) {
+	var buf bytes.Buffer
+	writer := zapcore.AddSync(&buf)
+
+	encoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "T",
+		LevelKey:       "L",
+		NameKey:        "N",
+		CallerKey:      "C",
+		MessageKey:     "M",
+		StacktraceKey:  "S",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	core := zapcore.NewCore(
+		zapcore.NewConsoleEncoder(encoderConfig),
+		writer,
+		zapcore.DebugLevel,
+	)
+
+	zapLogger := zap.New(core)
+	mgr := &logger.Manager{Zap: zapLogger}
+
+	ctx := context.WithValue(context.Background(), logger.TraceIDKey, "test-trace-id")
+
+	// Test all level logging
+	mgr.Info(ctx, "This is an info message")
+	mgr.Error(ctx, "This is an error message")
+	mgr.Debug(ctx, "This is a debug message")
+	mgr.Warn(ctx, "This is a warn message")
+
+	// Capture and assert the logs
+	mgr.Sync()
+
+	logs := buf.String()
+	assert.Contains(t, logs, "INFO")
+	assert.Contains(t, logs, "ERROR")
+	assert.Contains(t, logs, "DEBUG")
+	assert.Contains(t, logs, "WARN")
 }
